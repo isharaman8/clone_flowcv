1115dda396ae33d048ae06a191b3ca3c682d31b4
Aman Kumar <isharaman8@gmail.com>
2023-06-24 16:04:53 +0530

    update next config

--- frontend/flowcv_client/next.config.js
+++ frontend/flowcv_client/next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    env: {
+        NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    },
 };
 
 module.exports = nextConfig;
